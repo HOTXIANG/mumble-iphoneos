d76bb5ee78729ba3d801d30de881ff7a9a184229
HOTXIANG <hotxiang233@gmail.com>
2025-12-09 00:54:51 +0800

    Remove deprecated classes and files related to the welcome screen and popover background view. Introduce new SwiftUI components for PTT button, toast view, and preferences model and view.

--- Source/Classes/MUBackgroundView.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2014 The 'Mumble for iOS' Developers. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-@import UIKit;
-
-@interface MUBackgroundView : UIView
-+ (UIView *) backgroundView;
-@end
--- Source/Classes/MUMessageAttachmentViewController.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2009-2012 The 'Mumble for iOS' Developers. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-@import UIKit;
-
-@interface MUMessageAttachmentViewController : UITableViewController
-- (id) initWithImages:(NSArray *)images andLinks:(NSArray *)links;
-@end
