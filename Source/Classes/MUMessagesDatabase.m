982af9ad66ba947b44562381c85b3db393ea91c1
HOTXIANG <hotxiang233@gmail.com>
2026-07-20 22:13:32 +0800

    feat: 优化消息数据库的线程安全性和性能

--- Source/Classes/MUMessagesDatabase.m
+++ Source/Classes/MUMessagesDatabase.m
@@ -9,39 +9,68 @@
 #import <MumbleKit/MKTextMessage.h>
 
 #import <FMDB/FMDatabase.h>
+#import <dispatch/dispatch.h>
+
+static void *kMUMessagesDatabaseQueueKey = &kMUMessagesDatabaseQueueKey;
 
 @interface MUMessagesDatabase () {
     NSCache    *_msgCache;
     FMDatabase *_db;
     NSInteger  _count;
+    dispatch_queue_t _databaseQueue;
 }
+- (void)performDatabaseSync:(dispatch_block_t)block;
 @end
 
 @implementation MUMessagesDatabase
 
-- (id) init {
+- (id)init {
     if ((self = [super init])) {
+        _databaseQueue = dispatch_queue_create("cn.hotxiang.mumble.messages-database", DISPATCH_QUEUE_SERIAL);
+        dispatch_queue_set_specific(_databaseQueue, kMUMessagesDatabaseQueueKey, (__bridge void *)self, NULL);
+
         NSFileManager *manager = [NSFileManager defaultManager];
         NSString *directory = NSTemporaryDirectory();
         NSString *dbPath = [directory stringByAppendingPathComponent:@"msg.db"];
-        [manager removeItemAtPath:dbPath error:nil];
-        _db = [[FMDatabase alloc] initWithPath:dbPath];
-        if (![_db open]) {
-            MULogError(Database, @"MUMessagesDatabase: Failed to open.");
-        }
-        
-        [_db executeUpdate:@"CREATE TABLE IF NOT EXISTS `msg` "
-                           @"(`id` INTEGER PRIMARY KEY AUTOINCREMENT,"
-                           @" `rendered` BLOB,"
-                           @" `plist` BLOB)"];
-        
+
+        [self performDatabaseSync:^{
+            [manager removeItemAtPath:dbPath error:nil];
+            self->_db = [[FMDatabase alloc] initWithPath:dbPath];
+            if (![self->_db open]) {
+                MULogError(Database, @"MUMessagesDatabase: Failed to open.");
+            }
+
+            [self->_db executeUpdate:@"CREATE TABLE IF NOT EXISTS `msg` "
+                                      @"(`id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                      @" `rendered` BLOB,"
+                                      @" `plist` BLOB)"];
+        }];
+
         _msgCache = [[NSCache alloc] init];
         [_msgCache setCountLimit:10];
     }
     return self;
 }
 
-- (void) addMessage:(MKTextMessage *)msg withHeading:(NSString *)heading andSentBySelf:(BOOL)selfSent {
+- (void)dealloc {
+    [self performDatabaseSync:^{
+        [self->_db close];
+        self->_db = nil;
+    }];
+}
+
+- (void)performDatabaseSync:(dispatch_block_t)block {
+    if (!block) {
+        return;
+    }
+    if (dispatch_get_specific(kMUMessagesDatabaseQueueKey) == (__bridge void *)self) {
+        block();
+    } else {
+        dispatch_sync(_databaseQueue, block);
+    }
+}
+
+- (void)addMessage:(MKTextMessage *)msg withHeading:(NSString *)heading andSentBySelf:(BOOL)selfSent {
     NSError *err = nil;
     NSString *plainMsg = [msg plainTextString];
     plainMsg = [plainMsg stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
@@ -52,61 +81,78 @@
             [imageDataArray addObject:imgData];
         }
     }
-    
+
     NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:
-                                heading, @"heading",
-                                plainMsg, @"msg",
-                                [NSDate date], @"date",
-                                [msg embeddedLinks], @"links",
-                                imageDataArray, @"images",
-                                [NSNumber numberWithBool:selfSent], @"selfsent",
+                          heading, @"heading",
+                          plainMsg, @"msg",
+                          [NSDate date], @"date",
+                          [msg embeddedLinks], @"links",
+                          imageDataArray, @"images",
+                          [NSNumber numberWithBool:selfSent], @"selfsent",
                           nil];
     NSData *plist = [NSPropertyListSerialization dataWithPropertyList:dict format:NSPropertyListBinaryFormat_v1_0 options:0 error:&err];
-    [_db executeUpdate:@"INSERT INTO `msg` (`rendered`, `plist`) VALUES (?,?)", [NSNull null], plist ? plist : [NSNull null]];
-    _count++;
+
+    [self performDatabaseSync:^{
+        [self->_db executeUpdate:@"INSERT INTO `msg` (`rendered`, `plist`) VALUES (?,?)", [NSNull null], plist ? plist : [NSNull null]];
+        self->_count++;
+    }];
 }
 
-- (void) clearMessageAtIndex:(NSInteger)row {
-    [_db executeUpdate:@"UPDATE `msg` SET `plist`=NULL, `rendered`=NULL WHERE `id`=?", [NSNumber numberWithInteger:row+1]];
-    [_msgCache removeObjectForKey:[NSNumber numberWithInteger:row+1]];
+- (void)clearMessageAtIndex:(NSInteger)row {
+    [self performDatabaseSync:^{
+        [self->_db executeUpdate:@"UPDATE `msg` SET `plist`=NULL, `rendered`=NULL WHERE `id`=?", [NSNumber numberWithInteger:row + 1]];
+    }];
+    [_msgCache removeObjectForKey:[NSNumber numberWithInteger:row + 1]];
 }
 
-- (MUTextMessage *) messageAtIndex:(NSInteger)row {
-    MUTextMessage *txtMsg = [_msgCache objectForKey:[NSNumber numberWithInteger:row+1]];
-    if (txtMsg != nil)
-        return txtMsg;
-    
-    FMResultSet *result = [_db executeQuery:@"SELECT `plist` FROM `msg` WHERE `id` = ?", [NSNumber numberWithInteger:row+1]];
-    if ([result next]) {
-        NSData *plistData = [result dataForColumnIndex:0];
-        if (plistData) {
-            NSDictionary *dict = [NSPropertyListSerialization propertyListWithData:plistData options:0 format:nil error:nil];
-            if (dict) {
-                NSArray *imgDataArray = [dict objectForKey:@"images"];
-                NSMutableArray *imagesArray = [[NSMutableArray alloc] initWithCapacity:[imgDataArray count]];
-                for (NSData *data in imgDataArray) {
-#if TARGET_OS_IOS
-                    [imagesArray addObject:[UIImage imageWithData:data]];
-#else
-                    [imagesArray addObject:[[NSImage alloc] initWithData:data]];
-#endif
-                }
-                txtMsg = [MUTextMessage textMessageWithHeading:[dict objectForKey:@"heading"]
-                                                    andMessage:[dict objectForKey:@"msg"]
-                                              andEmbeddedLinks:[dict objectForKey:@"links"]
-                                             andEmbeddedImages:imagesArray
-                                              andTimestampDate:[dict objectForKey:@"date"]
-                                                  isSentBySelf:[[dict objectForKey:@"selfsent"] boolValue]];
-                [_msgCache setObject:txtMsg forKey:[NSNumber numberWithInteger:row+1]];
-                return txtMsg;
+- (MUTextMessage *)messageAtIndex:(NSInteger)row {
+    MUTextMessage *cachedMessage = [_msgCache objectForKey:[NSNumber numberWithInteger:row + 1]];
+    if (cachedMessage != nil) {
+        return cachedMessage;
+    }
+
+    __block NSDictionary *dict = nil;
+    [self performDatabaseSync:^{
+        FMResultSet *result = [self->_db executeQuery:@"SELECT `plist` FROM `msg` WHERE `id` = ?", [NSNumber numberWithInteger:row + 1]];
+        if ([result next]) {
+            NSData *plistData = [result dataForColumnIndex:0];
+            if (plistData) {
+                dict = [NSPropertyListSerialization propertyListWithData:plistData options:0 format:nil error:nil];
             }
         }
+        [result close];
+    }];
+
+    if (!dict) {
+        return nil;
     }
-    return nil;
+
+    NSArray *imgDataArray = [dict objectForKey:@"images"];
+    NSMutableArray *imagesArray = [[NSMutableArray alloc] initWithCapacity:[imgDataArray count]];
+    for (NSData *data in imgDataArray) {
+#if TARGET_OS_IOS
+        [imagesArray addObject:[UIImage imageWithData:data]];
+#else
+        [imagesArray addObject:[[NSImage alloc] initWithData:data]];
+#endif
+    }
+
+    MUTextMessage *txtMsg = [MUTextMessage textMessageWithHeading:[dict objectForKey:@"heading"]
+                                                        andMessage:[dict objectForKey:@"msg"]
+                                                  andEmbeddedLinks:[dict objectForKey:@"links"]
+                                                 andEmbeddedImages:imagesArray
+                                                  andTimestampDate:[dict objectForKey:@"date"]
+                                                      isSentBySelf:[[dict objectForKey:@"selfsent"] boolValue]];
+    [_msgCache setObject:txtMsg forKey:[NSNumber numberWithInteger:row + 1]];
+    return txtMsg;
 }
 
-- (NSInteger) count {
-    return _count;
+- (NSInteger)count {
+    __block NSInteger count = 0;
+    [self performDatabaseSync:^{
+        count = self->_count;
+    }];
+    return count;
 }
 
 @end
